cbca6cb11d8299d6e0f29b7b8e28fac7ee1f69b6
Martí Palazón Rovira <39364745+palazon12@users.noreply.github.com>
2019-04-30 13:18:51 +0200

    Add files via upload

---
+++ Ataque.js
@@ -0,0 +1,9 @@
+﻿function Update () {
+if(Input.GetButtonDown("Fire1"))
+{
+animation.CrossFade("Ataque2");
+}else if(Input.GetButtonDown("Fire2"))
+{
+animation.CrossFade("Ataque1");
+}
+}
---
+++ SaludJugador.js
@@ -0,0 +1,50 @@
+﻿var saludActual=100;
+var SaludMaxima=100.00;
+
+var largoBarra=0.0;
+
+
+function Start () {
+largoBarra=Screen.width/2;
+}
+
+function Update()
+{
+AjustarSalud(0);
+
+
+
+
+}
+function OnGUI()
+{
+GUI.Box(new Rect(10,30,largoBarra,20),saludActual+'/'+SaludMaxima);
+
+}
+
+function AjustarSalud(salud)
+{
+saludActual+=salud;
+
+
+if(saludActual<0)
+{
+
+saludActual=0;
+} 
+ if(saludActual>SaludMaxima)
+ {
+
+saludActual=SaludMaxima;
+}
+
+if(SaludMaxima<1){
+SaludMaxima=1;
+
+}
+
+largoBarra=(Screen.width/2)*saludActual/SaludMaxima;
+
+}
+
+
